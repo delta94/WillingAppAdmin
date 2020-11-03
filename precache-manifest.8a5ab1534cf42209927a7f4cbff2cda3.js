2ecd02099f29cb99535b30182eee1025abc16f06
Shachar Ganot <Shachar87@gmail.com>
2020-11-03 13:20:07 +0200

    Updates

---
+++ precache-manifest.8a5ab1534cf42209927a7f4cbff2cda3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a78e6f394a6addfb63bf61024078916",
+    "url": "/WillingAppAdmin/index.html"
+  },
+  {
+    "revision": "300175236840df64e220",
+    "url": "/WillingAppAdmin/static/css/2.87ff213f.chunk.css"
+  },
+  {
+    "revision": "dfcdaaba7d7984d2531e",
+    "url": "/WillingAppAdmin/static/css/main.2b6cd205.chunk.css"
+  },
+  {
+    "revision": "300175236840df64e220",
+    "url": "/WillingAppAdmin/static/js/2.b0e61f20.chunk.js"
+  },
+  {
+    "revision": "99e9bc6c81acb604904fe4c487fd0b3c",
+    "url": "/WillingAppAdmin/static/js/2.b0e61f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcdaaba7d7984d2531e",
+    "url": "/WillingAppAdmin/static/js/main.ecb8903a.chunk.js"
+  },
+  {
+    "revision": "0cad4ba00758fd029d44",
+    "url": "/WillingAppAdmin/static/js/runtime-main.89ae3d7c.js"
+  },
+  {
+    "revision": "f1d479accad3002a8ae2c51b77b53569",
+    "url": "/WillingAppAdmin/static/media/Dots.f1d479ac.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/WillingAppAdmin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/WillingAppAdmin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/WillingAppAdmin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/WillingAppAdmin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/WillingAppAdmin/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
