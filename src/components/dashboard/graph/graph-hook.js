5ef7c65e0c37344078acd43dfb1720ac841acdb3
amabelle <amabelletr@gmail.com>
2020-11-08 12:25:34 +0200

    check

--- src/components/dashboard/graph/graph-hook.js
+++ src/components/dashboard/graph/graph-hook.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import CanvasJSReact from '../../../assets/canvasjs.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
